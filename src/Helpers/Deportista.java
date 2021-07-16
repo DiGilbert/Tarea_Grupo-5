eee582604c0d713bd664984ae0062ba45486bd3d
erazomaycol <erazomike30@gmail.com>
2021-07-16 11:40:48 -0600

    Maycol Erazo V.1.2(subclase deportista)

--- src/Helpers/Deportista.java
+++ src/Helpers/Deportista.java
@@ -7,9 +7,53 @@ package Helpers;
 
 /**
  *
- * autor:Maycol_Erazo_
+* @author MaycoL_Moises_Erazo
  */
+public class Deportista extends Persona {
 
- public class Deportista {
-
+    private String deporte_ejercido;
+    private int años_de_carrera;
+    
+    
+    
+    public Deportista(){
+    establecerNombre("Mike Moises Jordan");
+    establecerEdad(32);
+    EstablecerPaisOrigen("Brasil");
+    establecerFecha_nacimiento("17 de Marzo de 1989");
+   
+    }
+    public void EstablecerDeporte_ejercido(String deporte){
+    this.deporte_ejercido=deporte; 
+    
+    
+    }
+    public String obtenerDeporte_ejercido(){
+    return this.deporte_ejercido;
+    
+    }
+    public void EstablecerAñosCarrera(int años_c){
+    this.años_de_carrera=años_c; 
+    
+    
+    }
+    public int obtenerAñosCarrera(){
+    return this.años_de_carrera;
+    
+    }
+    
+    
+    @Override
+   public String obtenerInformacionPersona() {
+     return "Datos de Deportista destacado";
+   
+     
+    }
+    public void ImprimirInformacionDeportista(){
+    
+      System.out.println("Deporte que realizado: "+ deporte_ejercido);
+      System.out.println("Años de carrera deportiva: "+ años_de_carrera);
+   
+   
+    }
 }
