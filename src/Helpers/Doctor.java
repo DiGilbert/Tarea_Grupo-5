5b708f8eaf9b1c876893f375ac1bb669d180045b
erazomaycol <erazomike30@gmail.com>
2021-07-16 11:42:19 -0600

    Alejandro Avila V.1.3(subclase doctor)

--- src/Helpers/Doctor.java
+++ src/Helpers/Doctor.java
@@ -6,8 +6,62 @@
 package Helpers;
 
 /**
+ *Alejandro Avila
  *
- * autor:Alejandro Avila.
  */
-public class Doctor {
+public class Doctor extends Persona {
+
+    
+    private String institucion_universitaria;
+    private String especialidad;
+  
+    private int años_de_experiencia;
+    
+    public Doctor(){
+    establecerNombre("Mario Franco Solorzano");
+    establecerEdad(36);
+    EstablecerPaisOrigen("Cuba");
+    establecerFecha_nacimiento("16 de Enero de 1985");
+    }
+    public void EstablecerInstitucion(String institucion){
+    this.institucion_universitaria=institucion; 
+    }
+    public String obtenerInstitucion(){
+    return this.institucion_universitaria;
+    
+    }
+    public void EstablecerEspecialidad(String especialidad_){
+    this.especialidad=especialidad_; 
+    
+    
+    }
+    public String obtenerEspecialidad(){
+    return this.especialidad;
+    
+    }
+     public void EstablecerAñosExperiencia(int años_experiencia){
+    this.años_de_experiencia=años_experiencia; 
+    
+    
+    }
+    public int obtenerAñosExperiencia(){
+    return this.años_de_experiencia;
+    
+    }
+   public void ImprimirInformacionDoctor(){
+    
+      System.out.println("Institución Universitaria: "+ institucion_universitaria);
+      System.out.println("Especialidad: "+ especialidad);
+      System.out.println("Años de experiencia: "+años_de_experiencia);
+     
+   
+    }
+    
+    @Override
+  public  String obtenerInformacionPersona() {
+       return "Datos de Doctor destacado";
+
+     
+    }
+    
 }
