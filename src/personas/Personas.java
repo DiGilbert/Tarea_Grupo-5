c63dd91c16d00946dd9b6defe0cb44f99ca0a6d8
erazomaycol <erazomike30@gmail.com>
2021-07-16 11:43:29 -0600

    Maycol Erazo V.1.4(metodo principal)

--- src/personas/Personas.java
+++ src/personas/Personas.java
@@ -4,11 +4,14 @@
  * and open the template in the editor.
  */
 package personas;
+
+import Helpers.Deportista;
+import Helpers.Doctor;
+
 /**
  *
- * @author MaycoL_Moises_Erazo_
+ * @author MaycoL_Moises_Erazo
  */
-
 public class Personas {
 
     /**
@@ -17,6 +20,27 @@ public class Personas {
     public static void main(String[] args) {
         // TODO code application logic here
                
+        Deportista depor= new Deportista();
+       
+        System.out.println(depor.obtenerInformacionPersona());
+        System.out.println("\n");
+        depor.imprimirInformacionP();
+        depor.EstablecerDeporte_ejercido("Tennis");
+        depor.EstablecerAñosCarrera(4);
+ 
+        depor.ImprimirInformacionDeportista();
+        
+        System.out.println("\n");
+        Doctor doc= new Doctor();
+               
+        System.out.println(doc.obtenerInformacionPersona());
+        System.out.println("\n");
+        doc.imprimirInformacionP();
+        doc.EstablecerInstitucion("Universidad de Ciencias Médicas de la Habana");
+        doc.EstablecerEspecialidad("Odontología");
+        doc.EstablecerAñosExperiencia(8);
+        doc.ImprimirInformacionDoctor();
+        
     }
     
 }
